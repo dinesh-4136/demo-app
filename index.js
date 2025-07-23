5254cd8eaa609369579e3a9d5c5fc9db180f79da
dinesh-4136 <r.dinesh4136@gmail.com>
2025-07-23 15:41:47 +0000

    Dockerfile added

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-// index.js 
-console.log("Hello from Dockerized App!");
+// index.js
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.end("Hello from Dockerized App!");
+});
+
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
+;
